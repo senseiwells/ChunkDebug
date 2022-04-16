434d90357432446375445f36f6155ce6df47cbb1
Sensei <wellsm488@gmail.com>
2022-04-16 13:17:06 +0100

    Level with 1.16.5

--- src/main/java/chunkdebug/utils/ChunkData.java
+++ src/main/java/chunkdebug/utils/ChunkData.java
@@ -3,15 +3,18 @@ package chunkdebug.utils;
 import net.minecraft.server.world.ChunkHolder;
 import net.minecraft.server.world.ChunkTicketType;
 import net.minecraft.util.math.ChunkPos;
+import net.minecraft.world.chunk.ChunkStatus;
 
 public class ChunkData {
 	private final ChunkPos chunkPos;
 	private final ChunkHolder.LevelType levelType;
+	private final byte statusCode;
 	private final byte ticketCode;
 
-	public ChunkData(ChunkPos chunkPos, ChunkHolder.LevelType levelType, ChunkTicketType<?> ticketType) {
+	public ChunkData(ChunkPos chunkPos, ChunkHolder.LevelType levelType, ChunkStatus status, ChunkTicketType<?> ticketType) {
 		this.chunkPos = chunkPos;
 		this.levelType = levelType;
+		this.statusCode = (byte) status.getIndex();
 		this.ticketCode = getTicketCode(ticketType);
 	}
 
@@ -27,26 +30,30 @@ public class ChunkData {
 		return (byte) this.levelType.ordinal();
 	}
 
+	public byte getStatusByte() {
+		return this.statusCode;
+	}
+
 	public byte getTicketByte() {
 		return this.ticketCode;
 	}
 
 	private static byte getTicketCode(ChunkTicketType<?> chunkTicketType) {
-		if (chunkTicketType == null) {
-			return 0;
+		if (chunkTicketType != null) {
+			return (byte) switch (chunkTicketType.toString()) {
+				default -> 0;
+				case "start" -> 1;
+				case "dragon" -> 2;
+				case "player" -> 3;
+				case "forced" -> 4;
+				case "light" -> 5;
+				case "portal" -> 6;
+				case "post_teleport" -> 7;
+				case "chonk" -> 8;
+				case "unknown" -> 9;
+			};
 		}
-		return (byte) switch (chunkTicketType.toString()) {
-			default -> 0;
-			case "start" -> 1;
-			case "dragon" -> 2;
-			case "player" -> 3;
-			case "forced" -> 4;
-			case "light" -> 5;
-			case "portal" -> 6;
-			case "post_teleport" -> 7;
-			case "chonk" -> 8;
-			case "unknown" -> 9;
-		};
+		return 0;
 	}
 
 	@Override
