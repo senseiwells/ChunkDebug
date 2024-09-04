91368b841ae83d27d47be17e3a5e413b64c898e2
senseiwells <wellsm488@gmail.com>
2024-09-04 12:37:57 +0100

    Fix no-shadow shader incompatibility

--- src/main/java/me/senseiwells/chunkdebug/client/mixins/GuiMixin.java
+++ src/main/java/me/senseiwells/chunkdebug/client/mixins/GuiMixin.java
@@ -1,10 +1,9 @@
 package me.senseiwells.chunkdebug.client.mixins;
 
-import com.llamalad7.mixinextras.sugar.Local;
 import me.senseiwells.chunkdebug.client.ChunkDebugClient;
-import net.minecraft.client.Minecraft;
+import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.gui.Gui;
-import net.minecraft.client.gui.LayeredDraw;
+import net.minecraft.client.gui.GuiGraphics;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,15 +11,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Gui.class)
 public class GuiMixin {
+	// We do this instead of adding a new layer for shader compatability reasons
 	@Inject(
-		method = "<init>",
-		at = @At(
-			value = "INVOKE",
-			target = "Lnet/minecraft/client/gui/LayeredDraw;add(Lnet/minecraft/client/gui/LayeredDraw;Ljava/util/function/BooleanSupplier;)Lnet/minecraft/client/gui/LayeredDraw;",
-			ordinal = 0
-		)
+		method = "renderEffects",
+		at = @At("TAIL")
 	)
-	private void addChunkDebugMinimapLayer(Minecraft minecraft, CallbackInfo ci, @Local(ordinal = 0) LayeredDraw layered) {
-		layered.add(ChunkDebugClient.getInstance()::onGuiRender);
+	private void renderChunkDebugMinimap(GuiGraphics graphics, DeltaTracker tracker, CallbackInfo ci) {
+		ChunkDebugClient.getInstance().onGuiRender(graphics, tracker);
 	}
 }
