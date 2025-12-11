a602290d823c297343c5c0d07a106346af271595
senseiwells <wellsm488@gmail.com>
2025-12-11 13:51:41 +0000

    chore: Version bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 	java
 }
 
-val modVersion = "2.5.1"
+val modVersion = "2.5.2"
 val releaseVersion = "${modVersion}+${libs.versions.minecraft.get()}"
 version = releaseVersion
 group = "me.senseiwells"
@@ -77,7 +77,7 @@ tasks {
 		file = remapJar.get().archiveFile
 		changelog.set(
 			"""
-			- Fixes a disconnection bug related to chunk generation statuses
+			- ChunkDebug will now remember the last minimap mode you had selected when joining a world
             """.trimIndent()
 		)
 		type = STABLE
